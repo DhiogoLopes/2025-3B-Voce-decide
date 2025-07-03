5040c0ddb164361935a4f333bb9d88165041fe0d
DhiogoLopes <141455193+DhiogoLopes@users.noreply.github.com>
2025-07-03 10:06:06 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,89 @@
+const caixaPrincipal = document.querySelector('.caixa-principal');
+const caixaPerguntas = document.querySelector('.caixa-perguntas');
+const caixaAlternativas = document.querySelector('.caixa-alternativas');
+const caixaResultado = document.querySelector('.caixa-principal');
+const textoResultado = document.querySelector('.texto-resultado');
+const perguntas = [
+
+    {
+        enunciado: "Pergunta 1",
+        alternativas: [
+            {
+                texto: "alternativa 01",
+                afirmacao: "resultado 01"
+            },
+            {
+                texto: "alternativa 02",
+                afirmacao: "resultado 02"
+            }
+        ]
+    },
+    {
+        enunciado: "Pergunta 2",
+        alternativas: [
+            {
+                texto: "alternativa 03",
+                afirmacao: "resultado 03"
+            },
+            {
+                texto: "alternativa 04",
+                afirmacao: "resultado04"
+            }
+        ]
+    },
+    {
+        enunciado: "Pergunta 3",
+        alternativas: [
+            {
+                texto: "alternativa 05",
+                afirmacao: "resultado 05"
+            },
+            {
+                texto: "alternativa 06",
+                afirmacao: "resultado 06"
+            }
+        ]
+    },
+    {
+        enunciado: "Pergunta 4",
+        alternativas: [
+            {
+                texto: "alternativa 07",
+                afirmacao: "resultado 07"
+            },
+            {
+                texto: "alternativa 08",
+                afirmacao: "resultado 08"
+            }
+        ]
+    }
+
+]
+
+
+let atual = 0;
+let perguntaAtual;
+
+function mostraPergunta() {
+    perguntaAtual = perguntas[atual]
+    caixaPerguntas.textContent = perguntaAtual.enunciado
+    mostraAlternativas()
+}
+
+function mostraAlternativas() {
+    for (const alternativa of perguntaAtual.alternativas) {
+        const botaoAlternativa = document.createElement("button");
+        botaoAlternativa.textContent = alternativa.texto;
+        botaoAlternativa.addEventListener("click", ()=> respotaSelecionada(alternativa))
+        caixaAlternativas.appendChild(botaoAlternativa);
+    }
+}
+
+function respotaSelecionada(opcaoSelecionada){
+    atual++
+    mostraPergunta();
+}
+
+mostraPergunta();
+
+console.log(perguntas)
